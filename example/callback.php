23b93b2025189a77b8d95c614ffb02e570a1582c
Denny Septian Panggabean <hexageek1337@gmail.com>
2020-12-18 22:16:52 +0700

    adding example response

--- example/callback.php
+++ example/callback.php
@@ -6,3 +6,26 @@ $apikey = ''; // input your api key to here
 $tripay = new Tripay($privatekey,$apikey);
 
 echo $tripay->callBack();
+
+/*
+* Example Response
+*
+{
+  "success": true,
+  "data": {
+    "reference": "DEV-T16240000002822I4QCZ",
+    "merchant_ref": "202012180001",
+    "payment_method": "Alfamart",
+    "payment_method_code": "ALFAMART",
+    "amount_received": "1000000",
+    "fee": "1250",
+    "total_amount": "1001250",
+    "is_customer_fee": "1",
+    "is_closed_payment": 1,
+    "status": "PAID",
+    "paid_at": "1608390880",
+    "note": "Testing yuks"
+  },
+  "event": "payment_status"
+}
+*/
